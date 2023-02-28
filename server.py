4ed592699ee60ed5b7852609d7d35731a613db97
jacobk189 <jacob.krueger@snc.edu>
2023-02-28 11:01:18 -0600

    changedserver

--- server.py
+++ server.py
@@ -11,17 +11,3 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 
 # Bind socket to local port number 12000
 serverSocket.bind(('', serverPort))
-
-
-# Loop forever
-while i<num:
-	# Read from UDP socket into message, getting client's 
-	# address (client IP and port)
-	#modifiedMessage = message.decode().upper()
-	
-	serverSocket.sendto(madlibwords[i].encode(), clientAddress)
-	
-	
-	#send upper case string back to this client
-	#serverSocket.sendto(modifiedMessage.encode(), clientAddress)
-    #this is jackson
