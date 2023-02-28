df536fe2c3cec1d2d50b2d4cd18e81294598e3c1
JacobShep <jacob.shepherd@snc.edu>
2023-02-28 10:39:41 -0600

    lab 4 code

--- client.py
+++ client.py
@@ -1,2 +1,11 @@
-#hi
-#jshep
+from socket import * #include Python's socket library
+import sys
+
+if len(sys.argv) != 4:
+    print('not enough arguments')
+
+serverName = sys.argv[1]
+serverPort = int(sys.argv[2])
+fileName = sys.argv[3]
+
+clientSocket = socket(AF_INET, SOCK_DGRAM)
