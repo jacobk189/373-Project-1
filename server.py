0cd90bdfe3345ed71b2f508d90b37433b50876ed
jacobk189 <jacob.krueger@snc.edu>
2023-02-28 10:40:29 -0600

    newserverfile

--- server.py
+++ server.py
@@ -1,2 +1,23 @@
-#hi
-#jackson is a bozo
+# UDPserver
+from socket import *
+import sys
+serverPort = 12000
+
+# Create UDP socket
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+
+# Bind socket to local port number 12000
+serverSocket.bind(('', serverPort))
+
+
+# Loop forever
+while i<num:
+	# Read from UDP socket into message, getting client's 
+	# address (client IP and port)
+	#modifiedMessage = message.decode().upper()
+	
+	serverSocket.sendto(madlibwords[i].encode(), clientAddress)
+	
+	
+	#send upper case string back to this client
+	#serverSocket.sendto(modifiedMessage.encode(), clientAddress)
