500f589c3adfa6866487ca98a705cae57716acdc
JacobShep <jacob.shepherd@snc.edu>
2023-02-28 10:56:11 -0600

    comment

--- server.py
+++ server.py
@@ -3,6 +3,9 @@ from socket import *
 import sys
 serverPort = 12000
 
+#connection_type = input('What type of connection are you running? \n')
+#do some if statments for each input
+
 # Create UDP socket
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 
