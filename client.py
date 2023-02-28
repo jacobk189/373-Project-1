27c96a4ade430d0cd4d0094a83f624b38fde3475
jcorcoran1 <91630698+jcorcoran1@users.noreply.github.com>
2023-02-28 11:17:24 -0600

    updated client file

--- client.py
+++ client.py
@@ -1,11 +1,26 @@
 from socket import * #include Python's socket library
 import sys
 
-if len(sys.argv) != 4:
-    print('not enough arguments')
+defaultFile = 'index.html'
+defaultPort = 12000
+defaultName = '127.0.0.1'
 
-serverName = sys.argv[1]
-serverPort = int(sys.argv[2])
-fileName = sys.argv[3]
+if len(sys.argv) == 4:
+    print('not enough arguments')
+    serverName = sys.argv[1]
+    serverPort = int(sys.argv[2])
+    fileName = sys.argv[3]
+
+else:
+    serverName = defaultName
+    serverPort = defaultPort
+    fileName = defaultFile
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket.sendto(fileName)
+msg, addr = clientSocket.recvfrom(2048)
+
+if(msg == 200):
+    clientSocket.recvfrom(2048)
+elif(msg == 404):
+    sys.exit()
